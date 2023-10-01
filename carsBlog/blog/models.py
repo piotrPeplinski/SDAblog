6d872f46beb11014630fab899bfcbc75bbfe7db3
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-01 15:43:20 +0200

    added image field to blog post model

--- carsBlog/blog/models.py
+++ carsBlog/blog/models.py
@@ -5,6 +5,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
     text = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return f'{self.date} | {self.title}'
