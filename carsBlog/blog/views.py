b3bf1ac500cc2dc03a24b807f925c583ab60e98f
Piotr Pepliński <p.peplaj@gmail.com>
2023-10-01 14:08:00 +0200

    created detail page

--- carsBlog/blog/views.py
+++ carsBlog/blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
+from django.http import Http404
 
 
 def home(request):
     posts = BlogPost.objects.all()
     return render(request, 'home.html', {'posts': posts})
+
+
+def detail(request, post_id):
+    # try:
+    #     post = BlogPost.objects.get(id=post_id)
+    # except BlogPost.DoesNotExist:
+    #     raise Http404(f'No post with id {post_id} was found.')
+    post = get_object_or_404(BlogPost, id=post_id)
+    return render(request, 'detail.html', {'post': post})
